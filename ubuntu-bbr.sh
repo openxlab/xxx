0c56adbd3e124e17282e54f1287844cd6dc0d2ae
openxlab <41830636+openxlab@users.noreply.github.com>
2019-02-20 21:53:01 +0800

    Create ubuntu-bbr.sh

---
+++ ubuntu-bbr.sh
@@ -0,0 +1,17 @@
+sudo apt update
+sudo apt list --upgradable
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt install -y python-pip
+pip --version
+
+
+sudo pip install shadowsocks
+sudo /usr/bin/python /usr/local/bin/ssserver -s 0.0.0.0 -p 9443 -k sss2019 -m aes-256-cfb --user nobody -d start &
+
+
+sudo echo "net.core.default_qdisc=fq" | sudo tee --append /etc/sysctl.conf
+sudo echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee --append /etc/sysctl.conf
+sudo sysctl -p
+sudo sysctl net.ipv4.tcp_available_congestion_control
+sudo lsmod | grep bbr
