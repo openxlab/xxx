f4feab143ab3a4d47b951d48b76bd89ba234b550
openxlab <41830636+openxlab@users.noreply.github.com>
2019-06-06 08:56:09 +0000

    Create ocp.sh

---
+++ ocp.sh
@@ -0,0 +1,3 @@
+INFO To access the cluster as the system:admin user when using 'oc', run 'export KUBECONFIG=/home/ubuntu/ocp/install/auth/kubeconfig'
+INFO Access the OpenShift web-console here: https://console-openshift-console.apps.ocp.openxcore.com
+INFO Login to the console with user: kubeadmin, password: kVdmo-5EgdF-izIrJ-znTtK
