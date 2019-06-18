8e3fe422451eb789549b6991322693635862f83c
openxlab <41830636+openxlab@users.noreply.github.com>
2019-06-18 07:38:55 +0000

    Update ubuntu-bbr.sh

--- ubuntu-bbr.sh
+++ ubuntu-bbr.sh
@@ -21,3 +21,16 @@ curl -sL -O https://github.com/Wind4/vlmcsd/releases/download/svn1112/binaries.t
 tar -xvf binaries.tar.gz
 chmod u+x ./binaries/Linux/intel/static/vlmcsd-x64-musl-static
 ./binaries/Linux/intel/static/vlmcsd-x64-musl-static
+
+sudo apt-get install -y unzip
+curl -L -s -O https://github.com/v2ray/v2ray-core/releases/download/v4.9.0/v2ray-linux-64.zip
+mkdir -p ~/v2ray
+sudo unzip v2ray-linux-64.zip -d ~/v2ray
+sudo chmod +x ~/v2ray/v2ray
+sudo chmod +x ~/v2ray/v2ctl
+sudo cp ~/v2ray/vpoint_vmess_freedom.json ~/v2ray/vmess_config.json
+sudo sed -i "s/10086/10886/g" ~/v2ray/vmess_config.json
+UUID=$(cat /proc/sys/kernel/random/uuid)
+sudo sed -i "s/23ad6b10-8d1a-40f7-8ad0-e3e35cd38297/${UUID}/g" ~/v2ray/vmess_config.json
+export PATH=$PATH:~/v2ray
+sudo ~/v2ray/v2ray -config ~/v2ray/vmess_config.json &
